9c3f977574c876a85a108ba1316b3781a70c2170
Kevin O <Kevinnuricumbo@gmail.com>
2022-02-03 12:05:53 -0600

    Actualizado

--- src/view/automata.rs
+++ src/view/automata.rs
@@ -15,6 +15,7 @@ pub mod automata{
     impl ValidarCFE {
         pub fn estado_0(&mut self){
             self.valid = String::from("2");
+            //Valida primer digito
             if self.text_entry.len() == 30 && !self.text_entry.contains(" ") {
                 if self.slide_entry[0..1].trim() == "0"{
                     self.slide_entry = self.slide_entry[1..].to_string();
@@ -31,6 +32,7 @@ pub mod automata{
         fn estado_1(&mut self);
     }
 
+    //Valida segundo digito
     impl E1 for ValidarCFE{
         fn estado_1(&mut self){
             if self.slide_entry[0..1].trim() == "1" {
@@ -45,6 +47,7 @@ pub mod automata{
     }
 
     impl E2 for ValidarCFE{
+        //validar los 12 digitos de servicio
         fn estado_2(&mut self) {
             let self_number: Result<i64,_> = self.slide_entry[..12].trim().parse();
             match self_number {
@@ -79,10 +82,19 @@ pub mod automata{
     }
 
     impl E3 for ValidarCFE{
+        //validar año
         fn estado_3(&mut self) {
-            if self.slide_entry[..2].to_string() == "00" || ((self.slide_entry[..2].parse::<i32>().unwrap()) >=1 && self.slide_entry[..2].parse::<i32>().unwrap()<=22 ){
-                self.slide_entry = self.slide_entry[2..].to_string();
-                self.estado_4();
+            let mut self_fecha: Result<i64,_> = self.slide_entry[..6].trim().parse();
+            match self_fecha {
+                Ok(_v) => {
+                    if self.slide_entry[..2].to_string() == "00" || ((self.slide_entry[..2].parse::<i32>().unwrap()) >=1 && self.slide_entry[..2].parse::<i32>().unwrap()<=22 ){
+                        self.slide_entry = self.slide_entry[2..].to_string();
+                        self.estado_4();
+                    }
+                },
+                Err(_e) => {
+                    println!("No es un numero valido lo siento :(")
+                }
             }
         }
     }
@@ -92,6 +104,7 @@ pub mod automata{
     }
 
     impl E4 for ValidarCFE{
+        //Validar mes
         fn estado_4(&mut self) {
             if self.slide_entry[..2].to_string() == "02"{
                 self.slide_entry = self.slide_entry[2..].to_string();
@@ -108,6 +121,7 @@ pub mod automata{
     }
 
     impl E5 for ValidarCFE{
+        //validar dia
         fn estado_5(&mut self, feb: bool) {
 
             if !feb && self.slide_entry[..2].parse::<i32>().unwrap() >=1 && self.slide_entry[..2].parse::<i32>().unwrap() <= 31{
@@ -115,7 +129,7 @@ pub mod automata{
                 self.slide_entry = self.slide_entry[2..].to_string();
                 self.estado_6();
             } else if self.slide_entry[..2].parse::<i32>().unwrap() >=1 && self.slide_entry[..2].parse::<i32>().unwrap() <= 29 {
-                println!("Estado 5Feb: {}", self.slide_entry); 
+                println!("Estado 5 Feb: {}", self.slide_entry); 
                 self.slide_entry = self.slide_entry[2..].to_string();
                 self.estado_6();
             }
@@ -129,16 +143,25 @@ pub mod automata{
     }
 
     impl E6 for ValidarCFE{
+        //validar importe
         fn estado_6(&mut self) {
-            let mut bandera = 0;
-            for item in 0..9{
-                if NUMBERS2.contains(self.slide_entry[item..item+1].trim()){
-                    bandera += 1;
+            let mut self_importe: Result<i64,_> = self.slide_entry[..9].trim().parse();
+            match self_importe {
+                Ok(_v) => {
+                    let mut bandera = 0;
+                    for item in 0..9{
+                        if NUMBERS2.contains(self.slide_entry[item..item+1].trim()){
+                            bandera += 1;
+                        }
+                    }
+                    if bandera == 9{
+                        self.slide_entry = self.slide_entry[9..].to_string();
+                        self.estado_7();
+                    }
+                },
+                Err(_e) => {
+                    println!("No es un numero valido lo siento :(")
                 }
-            }
-            if bandera == 9{
-                self.slide_entry = self.slide_entry[9..].to_string();
-                self.estado_7();
             }
         }
     }
@@ -148,6 +171,7 @@ pub mod automata{
     }
 
     impl E7 for ValidarCFE{
+        //validar ultimo digito
         fn estado_7(&mut self) {
             if NUMBERS2.contains(self.slide_entry[..1].trim()){
                 self.estado_8();
